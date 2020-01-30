e88e807b032ba7bbfab61649a759aee67c3d3635
Charlie Hu <hzy1324@qq.com>
2020-01-30 14:05:50 +0800

    :new: codeup 1818

---
+++ PAT/gcd.cpp
@@ -0,0 +1,20 @@
+// codeup 1818
+#include <cstdio>
+using namespace std;
+
+int gcd(int first, int second)
+{
+    if (second == 0)
+        return first;
+    return gcd(second, first % second);
+}
+
+int main()
+{
+    int first, second;
+    while (scanf("%d %d", &first, &second) != EOF)
+    {
+        printf("%d\n", gcd(first, second));
+    }
+    return 0;
+}
